e0fbdebb2bb4d2613fd2155cd8cbfe939921ef72
amandeep <aman@gmail.com>
2020-02-03 11:37:59 +0530

    assignment4

--- assignment4/header.php
+++ assignment4/header.php
@@ -9,11 +9,11 @@
         <div class="right">
             <div class="top_search">
             	<form class="form-inline" action="#"> 
-                <div class="input-group"> 
+                <div class="input-group "> 
                     
-                   <label class="control-label satisfy" style="">Call us now! (+91)8798908789</label>
-                   <input type="text" class="form-control ml-2" 
-                            placeholder="search" > 
+                   <label class="control-label satisfy col-sm-6" style="">Call us now! (+91)8798908789</label>
+                   <input type="text" class="form-control col-sm-6" 
+                            placeholder="search" style="background: url('<?=$base_url?>images/top_search.png') no-repeat;background-position: 98%;" > 
                     
                 </div>
             	</form> 
@@ -22,7 +22,7 @@
             <button class="navbar-toggler hidden-md-up pull-xs-right" data-target="#collapsenav" data-toggle="collapse" type="button">
 	            ☰
 	        </button>
-           
+
             <div class="collapse navbar-collapse navbar-light" id="collapsenav"> 
                 <ul class="navbar-nav"> 
                     <li class="nav-item"> 
